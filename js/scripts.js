51463794df9723626200dd67bf485d5298857a05
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-02-21 21:55:35 -0500

    created loop

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,13 @@ pokemonList = [
     }
 ];
 
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    document.write(pokemon.name + " (height: " + pokemon.height + ")");
+}
+
+
+
 
 
 

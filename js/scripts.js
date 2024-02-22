1368ba445f0a85ad940f17ca1a7cc49f473bdc7c
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-02-21 22:14:58 -0500

    added conditional

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,20 @@ pokemonList = [
     }
 ];
 
+// Threshold height for special Pokémon
+let thresholdHeight = 1.0;
+
+// Loop through each Pokémon in pokemonList and write its name, height
 for (let i = 0; i < pokemonList.length; i++) {
     let pokemon = pokemonList[i];
-    document.write(pokemon.name + " (height: " + pokemon.height + ")");
+    let output = pokemon.name + " (height: " + pokemon.height + ")";
+    // Check if the height of the current Pokémon exceeds the threshold
+    if (pokemon.height > thresholdHeight) {
+        output += " - Wow, that's big!";
+    }
+    
+    // Write the output 
+    document.write(output + "<br>");
 }
 
 

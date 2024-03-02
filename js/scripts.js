cdee92cf7815479d00099488a67eff9e3bbfc1d0
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-03-02 12:39:36 -0500

    wrapped function around IIFE and getAll, add functions
    and added getAll(), add() functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,46 @@
-//'Bulbasaur', 'Ivysaur', 'Venusaur'
-let pokemonList = []
-
-pokemonList = [
-    {
+var pokemonRepository = (function() {
+    var pokemonList = [
+      {
         name: "Bulbasaur",
         height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
+        types: ["grass", "poison"],
+      },
+      {
         name: "Ivysaur",
         height: 1,
-        types: ['grass', 'poison']
-    },
-    {
+        types: ["grass", "poison"],
+      },
+      {
         name: "Venusaur",
         height: 2,
-        types: ['grass', 'poison']
+        types: ["grass", "poison"],
+      },
+    ];
+  
+    function getAll() {
+      return pokemonList;
     }
-];
-
-
-
-
-
-// Loop through each Pokémon in pokemonList and write its name, height
-pokemonList.forEach(function(pokemon) {
+  
+    function add(pokemon) {
+      // Check if the parameter is an object and has required properties
+      if (typeof pokemon === "object" && "name" in pokemon && "height" in pokemon && "types" in pokemon) {
+        pokemonList.push(pokemon);
+      } else {
+        console.error("Pokemon is not in the correct format.");
+      }
+    }
+  
+    return {
+      getAll: getAll,
+      add: add,
+    };
+  })();
+  
+  // Loop through each Pokémon in pokemonRepository and write its name, height
+  pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(pokemon.name + " (height: " + pokemon.height + ")<br>");
-});
-
-
+  });
+  
 
 
 

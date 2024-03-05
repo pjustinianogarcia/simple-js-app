6a5e6ac53e9558ffaefa7822e5caff0b19d92571
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-03-05 10:23:23 -0500

    created addListItem in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,85 @@
-var pokemonRepository = (function() {
-    var pokemonList = [
-      {
-        name: "Bulbasaur",
-        height: 0.7,
-        types: ["grass", "poison"],
-      },
-      {
-        name: "Ivysaur",
-        height: 1,
-        types: ["grass", "poison"],
-      },
-      {
-        name: "Venusaur",
-        height: 2,
-        types: ["grass", "poison"],
-      },
+// Pokemon list with properties inside IIFE
+
+var pokemonRepository = (function () {
+    let repository = [
+        {
+            name: "Bulbasaur",
+            height: 0.7,
+            types: ["grass", "poison"],
+        },
+        {
+            name: "Ivysaur",
+            height: 1,
+            types: ["grass", "poison"],
+        },
+        {
+            name: "Venusaur",
+            height: 2,
+            types: ["grass", "poison"],
+        },
     ];
-  
-    function getAll() {
-      return pokemonList;
-    }
-  
+
+// function to add pokemon
     function add(pokemon) {
-      // Check if the parameter is an object and has required properties
-      if (typeof pokemon === "object" && "name" in pokemon && "height" in pokemon && "types" in pokemon) {
-        pokemonList.push(pokemon);
-      } else {
-        console.error("Pokemon is not in the correct format.");
-      }
+
+        if (typeof pokemon === "object" &&
+            "name" in pokemon &&
+            "height" in pokemon &&
+            "types" in pokemon
+        ) {
+            repository.push(pokemon);
+        } else {
+            console.log("Pokemon is not in the correct format.");
+        }
     }
-  
+
+    function getAll() {
+        return repository;
+    }
+
+    // created list items as buttons
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        // event listener "click" for each button 
+        button.addEventListener("click", function () {
+            showDetails(pokemon);
+        });
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon);
+
+    }
+
     return {
-      getAll: getAll,
-      add: add,
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
     };
-  })();
-  
-  // Loop through each Pokémon in pokemonRepository and write its name, height
-  pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write(pokemon.name + " (height: " + pokemon.height + ")<br>");
-  });
-  
+})();
+
+// print out pokemon list
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({
+    name: "Bulbasaur", height: 0.7,
+    types: ["grass", "poison"]
+});
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
+
+
+
+
 
 
 
